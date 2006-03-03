7fc0dd61db201dcb229d61f56199f16f80de1e20
Keith Grennan <kgrennan@cpan.org>
2006-03-03 03:52:34 +0000

    Import of KGRENNAN/Bundle-SXIP-Homesite-1.0.0 from CPAN.
    gitpan-cpan-distribution: Bundle-SXIP-Homesite
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         KGRENNAN/Bundle-SXIP-Homesite-1.0.0.tar.gz
    gitpan-cpan-author:       KGRENNAN
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/SXIP/Homesite.pm
@@ -0,0 +1,57 @@
+package Bundle::SXIP::Homesite;
+
+use warnings;
+use strict;
+
+our $VERSION = '1.0.0';
+
+1; # End of Bundle::SXIP::Homesite
+
+__END__
+
+=head1 NAME
+
+Bundle::SXIP::Homesite
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::SXIP::Homesite'
+
+=head1 CONTENTS
+
+CGI::Session
+
+Class::DBI::mysql
+
+Digest::HMAC_SHA1
+
+Digest::MD5
+
+Digest::SHA1
+
+Iterator
+
+Module::Pluggable
+
+SXIP::Digest
+
+Spiffy
+
+Test::More
+
+Time::Piece::MySql
+
+UNIVERSAL::require
+
+URI
+
+YAML
+
+=head1 DESCRIPTION
+
+This bundle defines all prerequisites for Sxip's Perl Homesite.
+
+=head1 SEE ALSO
+
+L<CPAN/Bundles>
+
